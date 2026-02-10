a90f631cb51a27105bf2fe1687ddd769e703bda5
ifty64bit <ifty64bit@gmail.com>
2026-02-11 00:08:35 +0600

    feat: Migrate TanStack Router Vite plugin to `@tanstack/router-plugin` and update its configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [tailwindcss(), react(), TanStackRouterVite()],
+    plugins: [
+        tailwindcss(),
+        tanstackRouter({
+            target: "react",
+            autoCodeSplitting: true,
+        }),
+        react(),
+    ],
 });
